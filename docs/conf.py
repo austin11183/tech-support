5d3e8eee819157474d223f0e72fb3cbbff685209
Yi-Cong, Kuo <c4493124@gmail.com>
2017-12-28 10:52:30 +0800

    add setting file

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+# General configuration.
+author = 'Technical Support Team'
+copyright = '2017, Interactive Digital Technologies, GIS Division'
+exclude_patterns = ['_build']
+extensions = ['sphinxcontrib.disqus']  # Add to this list.
+master_doc = 'index'
+project = 'ArcGIS-Tech-Support-TW'
+release = '1.0'
+version = '1.0'
+
+# Options for extensions.
+disqus_shortname = 'arcgis-tech-support-tw'  # Add this line to conf.py.
